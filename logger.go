731c6cd437c590aa0dbdece469bc9ff317b587eb
user_name <example.gmail.com>
2024-02-28 21:06:58 +0100

    Add SetSeparator and SetTimePrecision

--- logger.go
+++ logger.go
@@ -18,33 +18,68 @@ var (
 	success = color("\033[1;32m%s\033[0m")
 )
 
+// separator is a type defined for representing a separator in the log messages.
+// It is used to separate different parts of a log message.
+var separator string = " | "
+
+// timePrecision is a type defined for representing the precision of seconds in the time format.
+type timePrecision string
+
+// Constants for different levels of time precision.
+const (
+	// MILLI represents millisecond precision in the time format.
+	MILLI timePrecision = ".000"
+	// MICRO represents microsecond precision in the time format.
+	MICRO timePrecision = ".000000"
+	// NANO represents nanosecond precision in the time format.
+	NANO timePrecision = ".000000000"
+)
+
 // timeFormat is a global variable that holds the format of the time string.
 var timeFormat string
 
+// language is a global variable that holds the current language setting for the logger.
+// It is used to determine the format of the timestamp in the log messages.
+// The default language is set to English ("en").
+var language string = "en"
+
 // SetLanguage is a function that sets the time format based on the provided language.
 // It takes a string representing the language and sets the time format accordingly.
-func SetLanguage(language string) {
-	switch language {
+func SetLanguage(lang string) {
+	switch lang {
 	case "en":
-		timeFormat = "01-02-2006 3:04 PM"
+		timeFormat = "01-02-2006 3:04:05 PM"
 	case "fr":
-		timeFormat = "02-01-2006 15:04"
+		timeFormat = "02-01-2006 15:04:05"
 	case "de":
-		timeFormat = "02.01.2006 15:04"
+		timeFormat = "02.01.2006 15:04:05"
 	case "zh":
-		timeFormat = "2006年01月02日 15:04"
+		timeFormat = "2006年01月02日 15:04:05"
 	case "ja":
-		timeFormat = "2006年01月02日 15:04"
+		timeFormat = "2006年01月02日 15:04:05"
 	case "ru":
-		timeFormat = "02.01.2006 15:04"
+		timeFormat = "02.01.2006 15:04:05"
 	case "es":
-		timeFormat = "02-01-2006 15:04"
+		timeFormat = "02-01-2006 15:04:05"
 	case "it":
-		timeFormat = "02-01-2006 15:04"
+		timeFormat = "02-01-2006 15:04:05"
 	case "ar":
-		timeFormat = "02/01/2006 15:04"
+		timeFormat = "02/01/2006 15:04:05"
 	default:
-		timeFormat = "2006-01-02 15:04"
+		timeFormat = "2006-01-02 15:04:05"
+	}
+	language = lang
+}
+
+// SetTimePrecision is a function that sets the time format to include seconds precision.
+// It takes a timePrecision value representing the precision of the seconds (MILLI, MICRO, NANO).
+// If the current language is English ("en"), it sets the time format to "01-02-2006 3:04:05" followed by the precision and " PM".
+// For other languages, it appends the precision to the current time format.
+func SetTimePrecision(precision timePrecision) {
+	if language == "en" {
+		timeFormat = "01-02-2006 3:04:05" + string(precision) + " PM"
+	} else {
+		timeFormat = timeFormat + string(precision)
 	}
 }
 
@@ -52,6 +87,15 @@ func SetLanguage(language string) {
 // It takes a string representing the custom format and sets the time format to it.
 func SetCustomTimeFormat(customFormat string) {
 	timeFormat = customFormat
+}
+
+// SetSeparator is a function that sets the separator for the log messages.
+// It takes a string representing the separator and sets the global variable 'separator' to it.
+// The separator is used to separate different parts of a log message.
+// The default separator is " | ".
+// Example: "2021-07-01 12:00:00 | Debug | main.go:15 | Server is running on localhost 8080"
+func SetSeparator(sep string) {
+	separator = sep
 }
 
 // includeFileName is a global variable that determines whether the file name should be included in the log.
@@ -82,9 +126,9 @@ func color(colorString string) func(message, level string, args ...any) string {
 		fileLine := getFileAndLine()
 		var fullMessage string
 		if !includeFileName {
-			fullMessage = fmt.Sprintf("%s: %-7s: %s", timestamp, level, formattedMessage)
+			fullMessage = fmt.Sprintf("%s%s %-7s%s %s", timestamp, separator, level, separator, formattedMessage)
 		} else {
-			fullMessage = fmt.Sprintf("%s: %-7s: %-15s: %s", timestamp, level, fileLine, formattedMessage)
+			fullMessage = fmt.Sprintf("%s%s %-7s%s %-15s%s %s", timestamp, separator, level, separator, fileLine, separator, formattedMessage)
 		}
 		msg := fmt.Sprintf(colorString, fullMessage)
 
